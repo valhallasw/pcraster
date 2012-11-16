ec653b78239b238cf5f923a0b3de306da6d2dd84
Kor de Jong <k.dejong@geo.uu.nl>
2012-11-16 14:34:13 +0100

    Refactorings

--- Environment/Scripts/make_pcraster_package.sh
+++ Environment/Scripts/make_pcraster_package.sh
@@ -24,6 +24,51 @@ pcraster_sources="$PCRASTER"
 
 external_prefix="$PCRTEAM_EXTERN"
 
+os=`uname -o`
+
+if [ $os == "GNU/Linux" ]; then
+    ld_library_path=$LD_LIBRARY_PATH
+    python_path=$PYTHONPATH
+fi
+
+
+function configure_dll_path() {
+    local project_name=$1
+    if [ $os == "GNU/Linux" ]; then
+        export LD_LIBRARY_PATH="$build_root/${project_name}_$build_type/bin:$LD_LIBRARY_PATH"
+    fi
+    # TODO Darwin
+    # TODO Cygwin
+}
+
+
+function reset_dll_path() {
+    if [ $os == "GNU/Linux" ]; then
+        export LD_LIBRARY_PATH=$ld_library_path
+    fi
+    # TODO Darwin
+    # TODO Cygwin
+}
+
+
+function configure_python_path() {
+    local project_name=$1
+    if [ $os == "GNU/Linux" ]; then
+        export PYTHONPATH="$build_root/${project_name}_$build_type/bin:$PYTHONPATH"
+    fi
+    # TODO Darwin
+    # TODO Cygwin
+}
+
+
+function reset_python_path() {
+    if [ $os == "GNU/Linux" ]; then
+        export PYTHONPATH=$python_path
+    fi
+    # TODO Darwin
+    # TODO Cygwin
+}
+
 
 function build_project() {
     local project_name=$1
@@ -70,18 +115,16 @@ function build_projects() {
         -DXSD_ROOT=$build_root/xsd_$build_type
         -DDAL_ROOT=$build_root/dal_$build_type
     "
-    ld_library_path=$LD_LIBRARY_PATH
-    python_path=$PYTHONPATH
-    export LD_LIBRARY_PATH="$build_root/pcrtree2_$build_type/bin:$LD_LIBRARY_PATH"
-    export PYTHONPATH="$build_root/pcrtree2_$build_type/bin:$PYTHONPATH"
+    configure_dll_path pcrtree2
+    configure_python_path pcrtree2
     build_project pcrtree2 "
         -DDEVENV_ROOT=$build_root/devenv_$build_type
         -DXSD_ROOT=$build_root/xsd_$build_type
         -DDAL_ROOT=$build_root/dal_$build_type
         -DRASTERFORMAT_ROOT=$build_root/raster_format_$build_type
     "
-    export LD_LIBRARY_PATH=$ld_library_path
-    export PYTHONPATH=$python_path
+    reset_dll_path
+    reset_python_path
     build_project data_assimilation "
         -DDEVENV_ROOT=$build_root/devenv_$build_type
     "
@@ -94,18 +137,16 @@ function install_projects() {
 
     install_project dal
     install_project aguila
-    ld_library_path=$LD_LIBRARY_PATH
-    python_path=$PYTHONPATH
-    export LD_LIBRARY_PATH="$build_root/pcrtree2_$build_type/bin:$LD_LIBRARY_PATH"
-    export PYTHONPATH="$build_root/pcrtree2_$build_type/bin:$PYTHONPATH"
+    configure_dll_path pcrtree2
+    configure_python_path pcrtree2
     install_project pcrtree2
-    export LD_LIBRARY_PATH=$ld_library_path
-    export PYTHONPATH=$python_path
+    reset_dll_path
+    reset_python_path
     install_project data_assimilation
 }
 
 
-# build_projects
+build_projects
 install_projects
 fixup.py $install_prefix $external_prefix
 verify_pcraster_installation.py $install_prefix
--- Environment/Scripts/make_pcraster_package_linux.sh
+++ Environment/Scripts/make_pcraster_package_linux.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+install_prefix=
+
 export MAKEFLAGS="-j4"
 
 build_type="Release"
-base_name="pcraster-`date +\"%Y%m%d\"`"
-base_name="pcraster-20121115"
+base_name="pcraster-`date +%Y%m%d`"
 build_root=`pwd`
 install_prefix=`pwd`/$base_name
 
 make_pcraster_package.sh "$build_root" "$install_prefix" $build_type
+tar zcf $base_name.tar.gz $base_name
+ls -lh $base_name.tar.gz
