315aa30593df83ef0f6f9299cb92f51a93c233a1
Kor de Jong <kor@jemig.eu>
2013-04-09 11:39:17 +0200

    Git repo stuff is now removed

--- environment/scripts/create_pcraster_source_distro.sh
+++ environment/scripts/create_pcraster_source_distro.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
-sourceforge_username=$1
-pcrserver_username=$2
-target_directory="pcraster-`date +%Y%m%d`"
-zip_name=$target_directory.tar.gz
+target_root_directory=$(readlink -f $1)
+sourceforge_username=$2
+pcrserver_username=$3
+target_basename="pcraster-`date +%Y%m%d`"
+zip_name=$target_basename.tar.gz
 
+# All commands are run from the target_root_directory passed in.
+cd $target_root_directory
+
+# Create target directory. It must not already exist. Remove lingering zip.
+mkdir $target_basename
 rm -f $zip_name
 
-# Create target directory. It must not already exist.
-mkdir $target_directory
-
-# Clone sources.
-clone_pcraster_sources.sh $target_directory $sourceforge_username $pcrserver_username
+# Clone sources and get rid of the repository stuff. Careful with the rm!
+clone_pcraster_sources.sh $target_basename $sourceforge_username $pcrserver_username
+find $target_basename -type d -name \.git -print | xargs rm -fr
+find $target_basename -type f -name \.gitignore -print | xargs rm -f
 
 # Copy license and build info to root of sources.
-cp $target_directory/devenv/documentation/licenses/pcraster.txt $target_directory/COPYING.TXT
-cp $target_directory/pcraster/documentation/pcraster/build.rst $target_directory/INSTALL.TXT
-cp $target_directory/pcraster/documentation/pcraster/changes.rst $target_directory/CHANGES.TXT
+cp $target_basename/devenv/documentation/licenses/pcraster.txt $target_basename/COPYING.TXT
+cp $target_basename/pcraster/documentation/pcraster/build.rst $target_basename/INSTALL.TXT
+cp $target_basename/pcraster/documentation/pcraster/changes.rst $target_basename/CHANGES.TXT
 
-# Zip sources.
-tar zcf $zip_name $target_directory
+# Zip sources and clean up.
+tar zcf $zip_name $target_basename
+rm -fr $target_basename
 
-# Clean.
-rm -fr $target_directory
+ls -l $zip_name
